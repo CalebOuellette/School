53c35ef16b2524c55d396537a230b4e0b573acb0
Caleb Ouellette <caleb.ouellette@gmail.com>
2018-04-27 15:40:08 -0400

    add

---
+++ 447/ps2/measureSystemCall.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+double timespec_to_ms(struct timespec *ts)
+{
+  return ts->tv_sec * 1000.0 + ts->tv_nsec / 1000000.0;
+}
+int main()
+{
+  struct timespec start_time, end_time;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+  /* begin timing */
+  /* end timing */
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+  printf("%f ms\n", timespec_to_ms(&end_time) - timespec_to_ms(&start_time));
+}

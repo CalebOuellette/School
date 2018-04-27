964961fae8d2ba61a4d7e4e012043d053357334b
Caleb Ouellette <caleb.ouellette@gmail.com>
2018-04-27 15:50:52 -0400

    three

--- 447/ps2/measureSystemCall.c
+++ 447/ps2/measureSystemCall.c
@@ -8,6 +8,11 @@
 *    a. WWU work machine 
 *    b. 4.5 ms
 *    c. 4.5 ns
+* 2. 
+*    a. 6.3 ms
+*    c. 6.3 ns
+* 3. 
+*    a.
 */
 
 void one()
@@ -25,6 +30,14 @@ void two()
   }
 }
 
+void three()
+{
+  for (int i = 0; i < 1000000; i++)
+  {
+    syscall(SYS_getpid);
+  }
+}
+
 double timespec_to_ms(struct timespec *ts)
 {
   return ts->tv_sec * 1000.0 + ts->tv_nsec / 1000000.0;
@@ -34,7 +47,9 @@ int main()
   struct timespec start_time, end_time;
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
   /* begin timing */
-  two();
+  //one();
+  //two();
+  three();
   /* end timing */
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
   printf("%f ms\n", timespec_to_ms(&end_time) - timespec_to_ms(&start_time));

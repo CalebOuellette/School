1c39cbca62d6fca7a68cf40d807795b600719505
Caleb Ouellette <caleb.ouellette@gmail.com>
2018-05-07 19:34:32 -0700

    all done.

--- 447/prj2/shell.c
+++ 447/prj2/shell.c
@@ -6,9 +6,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/param.h>
 
 #define COMMAND_LENGTH 1024
 #define NUM_TOKENS (COMMAND_LENGTH / 2 + 1)
+
+#define HISTORY_DEPTH 10
+char history[HISTORY_DEPTH][COMMAND_LENGTH];
+int historyStart = 0;
+int historyCount = 1;
+
+void add_to_history(char *command)
+{
+  historyCount++;
+  strcpy(history[historyStart], command);
+  historyStart = (historyStart + 1) % HISTORY_DEPTH;
+}
+
+void print_history()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    if ((10 - i) < historyCount)
+    {
+      int x = (i + historyStart) % 10;
+      char str[10];
+      sprintf(str, "%d", (historyCount - (10 - i)));
+      write(STDERR_FILENO, str, strlen(str));
+      write(STDERR_FILENO, "\t", strlen("\t"));
+      write(STDERR_FILENO, history[x], strlen(history[x]));
+      write(STDERR_FILENO, "\n", strlen("\n"));
+    }
+  }
+}
+
+char *get_command_by_history(int i)
+{
+  int x = historyStart - (historyCount - i);
+  return history[x];
+}
+
+void check_background(int token_count, char *tokens[], _Bool *in_background)
+{
+  if (token_count > 0 && strcmp(tokens[token_count - 1], "&") == 0)
+  {
+    *in_background = true;
+    tokens[token_count - 1] = 0;
+  }
+}
 
 /**
  * Command Input and Processing
@@ -25,7 +71,7 @@
  *       Ends with a null pointer.
  * returns: number of tokens.
  */
-int tokenize_command(char *buff, char *tokens[])
+int tokenize_command(char *buff, char *tokens[], _Bool *in_background)
 {
   int token_count = 0;
   _Bool in_token = false;
@@ -53,6 +99,7 @@ int tokenize_command(char *buff, char *tokens[])
     }
   }
   tokens[token_count] = NULL;
+  check_background(token_count, tokens, in_background);
   return token_count;
 }
 
@@ -67,6 +114,7 @@ int tokenize_command(char *buff, char *tokens[])
  * in_background: pointer to a boolean variable. Set to true if user entered
  *       an & as their last token; otherwise set to false.
  */
+
 void read_command(char *buff, char *tokens[], _Bool *in_background)
 {
   *in_background = false;
@@ -74,7 +122,7 @@ void read_command(char *buff, char *tokens[], _Bool *in_background)
   // Read input
   int length = read(STDIN_FILENO, buff, COMMAND_LENGTH - 1);
 
-  if (length < 0)
+  if (length < 0 && (errno != EINTR))
   {
     perror("Unable to read command from keyboard. Terminating.\n");
     exit(-1);
@@ -87,32 +135,149 @@ void read_command(char *buff, char *tokens[], _Bool *in_background)
     buff[strlen(buff) - 1] = '\0';
   }
 
+  if (buff[0] != '!')
+  {
+    add_to_history(buff);
+  }
+
   // Tokenize (saving original command string)
-  int token_count = tokenize_command(buff, tokens);
+  int token_count = tokenize_command(buff, tokens, in_background);
   if (token_count == 0)
   {
     return;
   }
+}
 
-  // Extract if running in background:
-  if (token_count > 0 && strcmp(tokens[token_count - 1], "&") == 0)
+/**
+ * forks the process and executes the child.
+**/
+void exec_child(char **tokens, _Bool in_background)
+{
+  const pid_t pid = fork();
+  if (pid == 0)
   {
-    *in_background = true;
-    tokens[token_count - 1] = 0;
+    int result = execvp(tokens[0], tokens);
+    if (result == -1)
+    {
+      write(STDERR_FILENO, strerror(errno), strlen(strerror(errno)));
+      write(STDERR_FILENO, "\n", strlen("\n"));
+    }
+  }
+  else if (pid == -1)
+  {
+    perror("fork");
+  }
+  else
+  {
+    if (!in_background)
+    {
+      int status;
+      waitpid(pid, &status, 0);
+    }
   }
 }
 
-void exec_child(char **tokens)
+/**
+ * Have you played left for dead?
+**/
+void kill_the_zombies()
 {
-  fork();
-  execvp(tokens[0], tokens);
+  // Cleanup any previously exited background child processes
+  // (The zombies)
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    ; // do nothing.
 }
 
+void print_cwd()
+{
+  char *path = malloc(MAXPATHLEN * sizeof(char *));
+  getcwd(path, MAXPATHLEN);
+  write(STDOUT_FILENO, path, strlen(path));
+  free(path);
+}
+
+void interpret_tokens(char **tokens, _Bool in_background)
+{
+  if (strcmp(tokens[0], "exit") == 0)
+  {
+    exit(EXIT_SUCCESS);
+  }
+  else if (strcmp(tokens[0], "pwd") == 0)
+  {
+    print_cwd();
+    write(STDOUT_FILENO, "\n", strlen("\n"));
+  }
+  else if (strcmp(tokens[0], "cd") == 0)
+  {
+    int err = chdir(tokens[1]);
+    if (err == -1)
+    {
+      write(STDERR_FILENO, strerror(errno), strlen(strerror(errno)));
+      write(STDERR_FILENO, "\n", strlen("\n"));
+    }
+  }
+  else if (strcmp(tokens[0], "history") == 0)
+  {
+    print_history();
+  }
+  else if (strcmp(tokens[0], "") == 0)
+  {
+  }
+  else if (tokens[0][0] == '!')
+  {
+    print_history();
+    if (tokens[0][1] == '!')
+    {
+      char *command = get_command_by_history(historyCount - 1);
+      add_to_history(command);
+      char *tokens[NUM_TOKENS];
+      _Bool in_background = false;
+      tokenize_command(command, tokens, &in_background);
+      exec_child(tokens, in_background);
+    }
+    else
+    {
+      char *num = tokens[0] + 1;
+      int i = atoi(num);
+      if (i == 0)
+      {
+        write(STDERR_FILENO, "Not a valid history number \n", strlen("Not a valid history number \n"));
+      }
+      else if (i >= historyCount)
+      {
+        write(STDERR_FILENO, "Not a valid history number \n", strlen("Not a valid history number \n"));
+      }
+      else
+      {
+        char *command = get_command_by_history(i);
+        add_to_history(command);
+        char *tokens[NUM_TOKENS];
+        _Bool in_background = false;
+        tokenize_command(command, tokens, &in_background);
+        exec_child(tokens, in_background);
+      }
+    }
+  }
+  else
+  {
+    exec_child(tokens, in_background);
+  }
+}
+
+void sig_handler(int i)
+{
+  write(STDOUT_FILENO, "\n", strlen("\n"));
+  print_history();
+  print_cwd();
+  write(STDOUT_FILENO, "> ", strlen("> "));
+}
 /**
  * Main and Execute Commands
  */
 int main(int argc, char *argv[])
 {
+  signal(SIGINT, sig_handler);
+
   char input_buffer[COMMAND_LENGTH];
   char *tokens[NUM_TOKENS];
   while (true)
@@ -121,10 +286,11 @@ int main(int argc, char *argv[])
     // Get command
     // Use write because we need to use read() to work with
     // signals, and read() is incompatible with printf().
+    print_cwd();
     write(STDOUT_FILENO, "> ", strlen("> "));
     _Bool in_background = false;
-    read_command(input_buffer, tokens, &in_background);
 
+    read_command(input_buffer, tokens, &in_background);
     // DEBUG: Dump out arguments:
     for (int i = 0; tokens[i] != NULL; i++)
     {
@@ -145,11 +311,8 @@ int main(int argc, char *argv[])
 		 *    child to finish. Otherwise, parent loops back to
 		 *    read_command() again immediately.
 		 */
-    exec_child(tokens);
-    if (!in_background)
-    {
-      wait(NULL);
-    }
+    kill_the_zombies();
+    interpret_tokens(tokens, in_background);
   }
   return 0;
 }

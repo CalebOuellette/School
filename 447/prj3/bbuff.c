60e5fcfd6edad81261ec966751b58b7efa00a76c
Caleb Ouellette <caleb.ouellette@gmail.com>
2018-05-21 17:26:47 -0700

    wrote buffer.

--- 447/prj3/bbuff.c
+++ 447/prj3/bbuff.c
@@ -1,10 +1,35 @@
 #include <stdbool.h>
-void bbuff_init(void){};
-void bbuff_blocking_insert(void *item){
+#include "bbuff.h"
+#include <pthread.h>
+int count = 0;
+int in = 0;
+int out = 0;
+void *buffer[BUFFER_SIZE];
 
-};
-void *bbuff_blocking_extract(void);
-_Bool bbuff_is_empty(void)
+pthread_mutex_t lock;
+
+void bbuff_init(void)
 {
-  return true;
+  pthread_mutex_init(&lock, NULL);
+};
+void bbuff_blocking_insert(void *item)
+{
+  pthread_mutex_lock(&lock);
+  while (count == BUFFER_SIZE)
+    ; // do nothing
+  buffer[in] = item;
+  in = (in + 1) % BUFFER_SIZE;
+  count++;
+  pthread_mutex_unlock(&lock);
+};
+void *bbuff_blocking_extract(void)
+{
+  pthread_mutex_lock(&lock);
+  while (count == 0)
+    ; // do nothing
+  void *nextConsumed = buffer[out];
+  out = (out + 1) % BUFFER_SIZE;
+  count--;
+  pthread_mutex_unlock(&lock);
+  return nextConsumed;
 };
--- 447/prj3/bbuff.h
+++ 447/prj3/bbuff.h
@@ -4,5 +4,4 @@
 void bbuff_init(void);
 void bbuff_blocking_insert(void *item);
 void *bbuff_blocking_extract(void);
-_Bool bbuff_is_empty(void);
 #endif
